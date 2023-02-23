bc3a7a29361a5c0a07d1f99e16cfac92a1aa2954
Oleg Melnik <olezko46@gmail.com>
2023-02-24 01:24:44 +0300

    Initial Commit

--- predict.py
+++ predict.py
@@ -2,16 +2,23 @@ import os
 from typing import List
 
 import torch
+
+from cog import BasePredictor, Input, Path
+
 from diffusers import (
     PNDMScheduler,
     LMSDiscreteScheduler,
     DDIMScheduler,
     StableDiffusionPipeline
 )
-from cog import BasePredictor, Input, Path
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker,
+)
 
 MODEL_ID = "XpucT/Deliberate"
 MODEL_CACHE = "diffusers-cache"
+
+SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
 
 
 class Predictor(BasePredictor):
@@ -19,8 +26,15 @@ class Predictor(BasePredictor):
         """Load the model into memory to make running multiple predictions efficient"""
         print("Loading pipeline...")
 
-        self.txt2img_pipe = StableDiffusionPipeline.from_pretrained(
+        safety_checker = StableDiffusionSafetyChecker.from_pretrained(
+            SAFETY_MODEL_ID,
+            cache_dir=MODEL_CACHE,
+            local_files_only=True,
+        )
+
+        self.pipe = StableDiffusionPipeline.from_pretrained(
             MODEL_ID,
+            safety_checker=safety_checker,
             cache_dir=MODEL_CACHE,
             local_files_only=True,
         ).to("cuda")
@@ -28,46 +42,46 @@ class Predictor(BasePredictor):
     @torch.inference_mode()
     @torch.cuda.amp.autocast()
     def predict(
-        self,
-        prompt: str = Input(description="Input prompt", default=""),
-        negative_prompt: str = Input(
-            description="Specify things to not see in the output",
-            default="",
-        ),
-        width: int = Input(
-            description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=512,
-        ),
-        height: int = Input(
-            description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=512,
-        ),
-        prompt_strength: float = Input(
-            description="Prompt strength when using init image. 1.0 corresponds to full destruction of information in init image",
-            default=0.8,
-        ),
-        num_outputs: int = Input(
-            description="Number of images to output. If the NSFW filter is triggered, you may get fewer outputs than this.",
-            ge=1,
-            le=10,
-            default=1
-        ),
-        num_inference_steps: int = Input(
-            description="Number of denoising steps", ge=1, le=500, default=50
-        ),
-        guidance_scale: float = Input(
-            description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
-        ),
-        scheduler: str = Input(
-            default="K-LMS",
-            choices=["DDIM", "K-LMS", "PNDM"],
-            description="Choose a scheduler",
-        ),
-        seed: int = Input(
-            description="Random seed. Leave blank to randomize the seed", default=None
-        ),
+            self,
+            prompt: str = Input(description="Input prompt", default=""),
+            negative_prompt: str = Input(
+                description="Specify things to not see in the output",
+                default="",
+            ),
+            width: int = Input(
+                description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
+                choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
+                default=512,
+            ),
+            height: int = Input(
+                description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
+                choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
+                default=512,
+            ),
+            prompt_strength: float = Input(
+                description="Prompt strength when using init image. 1.0 corresponds to full destruction of information in init image",
+                default=0.8,
+            ),
+            num_outputs: int = Input(
+                description="Number of images to output. If the NSFW filter is triggered, you may get fewer outputs than this.",
+                ge=1,
+                le=10,
+                default=1
+            ),
+            num_inference_steps: int = Input(
+                description="Number of denoising steps", ge=1, le=500, default=50
+            ),
+            guidance_scale: float = Input(
+                description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
+            ),
+            scheduler: str = Input(
+                default="K-LMS",
+                choices=["DDIM", "K-LMS", "PNDM"],
+                description="Choose a scheduler",
+            ),
+            seed: int = Input(
+                description="Random seed. Leave blank to randomize the seed", default=None
+            ),
     ) -> List[Path]:
         """Run a single prediction on the model"""
         if seed is None:
@@ -81,14 +95,12 @@ class Predictor(BasePredictor):
 
         extra_kwargs = {}
 
-        pipe = self.txt2img_pipe
-
-        pipe.scheduler = make_scheduler(scheduler)
+        self.pipe.scheduler = make_scheduler(scheduler)
 
         generator = torch.Generator("cuda").manual_seed(seed)
-        output = pipe(
+        output = self.pipe(
             prompt=[prompt] * num_outputs if prompt is not None else None,
-            negative_prompt=[negative_prompt]*num_outputs if negative_prompt is not None else None,
+            negative_prompt=[negative_prompt] * num_outputs if negative_prompt is not None else None,
             width=width,
             height=height,
             guidance_scale=guidance_scale,
@@ -99,8 +111,8 @@ class Predictor(BasePredictor):
 
         output_paths = []
         for i, sample in enumerate(output.images):
-            if output.nsfw_content_detected and output.nsfw_content_detected[i] and self.NSFW:
-                continue
+            # if output.nsfw_content_detected and output.nsfw_content_detected[i] and self.NSFW:
+            #     continue
 
             output_path = f"/tmp/out-{i}.png"
             sample.save(output_path)
